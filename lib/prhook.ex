9080c81a51d27a362f3aa7b52e21573200cfc2cc
bit <mushbeen25@gmail.com>
2025-11-01 22:46:07 +0900

    feat: add body parsing

--- lib/prhook.ex
+++ lib/prhook.ex
@@ -13,6 +13,32 @@ defmodule Prhook do
     end
   end
 
+  def parse_body(body) do
+    nodes = get_in(body, ["data", "search", "nodes"]) || []
+
+    nodes
+    |> Enum.group_by(&get_in(&1, ["repository", "nameWithOwner"]))
+    |> Enum.map(fn {repo_name, issues} ->
+      first_issue = List.first(issues)
+      repo_url = get_in(first_issue, ["repository", "url"])
+
+      %{
+        repository: %{
+          name: repo_name,
+          url: repo_url
+        },
+        issues:
+          Enum.map(issues, fn issue ->
+            %{
+              number: issue["number"],
+              title: issue["title"],
+              url: issue["url"]
+            }
+          end)
+      }
+    end)
+  end
+
   def hello do
     :world
   end
